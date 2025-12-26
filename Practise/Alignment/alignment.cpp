75dc83bd050245757e01ae6530b1a2b149a9a44b
grigoryaan <Grigoryanalen142@gmail.com>
2025-12-26 17:24:00 +0400

    Implement class 'coo' and display its size
    Added a class 'coo' with multiple data members and a main function to display its size.

--- Practise/Alignment/alignment.cpp
+++ Practise/Alignment/alignment.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+class coo {
+public:
+	int a;
+	int b;
+	double c;
+	char d;
 
+    coo() {}
+    ~coo() {}
+};
+int main()
+{
+    coo x;
+    std::cout << sizeof(x) << std::endl;       //  * 24  * //
+}
